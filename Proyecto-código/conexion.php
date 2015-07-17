8ed0ebb913fd616a59840d99ff77e63a72337583
Robert Caraguay <robertwladimirc@gmail.com>
2015-07-17 16:50:30 -0500

    Parámetros de conexión BD
    Corrección de los parámetros de conexión BD

--- Proyecto-código/conexion.php
+++ Proyecto-código/conexion.php
@@ -1,9 +1,9 @@
 <!--Conexión a la base de datos !-->
 <?php
-	$conexion = mysql_connect("localhost", "user","password");
-	mysql_database("nombre_base_datos",$conexion);
+	$conexion = mysql_connect("localhost", "root","1234");
+	mysql_database("matricula",$conexion);
 
-	$query = mysql_query("select * from tabla",$conexion);
+	$query = mysql_query("select * from usuarios",$conexion);
 
 	$resultado = ($query,0);
 
